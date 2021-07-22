8b8a4cbd64205cf5330c8125d56807eb9e53992e
Uptaker <markus.tammeoja@gmail.com>
2021-07-22 20:30:50 +0300

    Add controller

---
+++ src/main/java/codes/drinky/jokes/controllers/JokesController.java
@@ -0,0 +1,26 @@
+package codes.drinky.jokes.controllers;
+
+import codes.drinky.jokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
